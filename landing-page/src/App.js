24e6043e2710746522758b98009a5cd1e74d56c6
Sanek-2004 <iwanov.vany-ivanov@yandex.ru>
2023-06-12 17:21:30 +0300

    set up project

---
+++ landing-page/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <nav className="nav">
+        <div className="container">
+          <div className="nav__row">
+            <div className="logo">LOGO</div>
+            <div className="navigation">NAV</div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
