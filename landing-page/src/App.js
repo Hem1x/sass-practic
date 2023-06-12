8621e336a826ea43d6cd72749bd5920e6ded14ce
Sanek-2004 <iwanov.vany-ivanov@yandex.ru>
2023-06-12 18:34:34 +0300

    navigation is completed

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,11 +1,24 @@
+import logo from './assets/icons/logo.svg'
+
 function App() {
   return (
     <div>
       <nav className="nav">
         <div className="container">
           <div className="nav__row">
-            <div className="logo">LOGO</div>
-            <div className="navigation">NAV</div>
+            <div className="logo">
+              <img src={logo} alt="Logo"/>
+              <span>CutSpace</span>
+            </div>
+            <div className="navigation">
+              <ul>    
+                <li><a className="active" href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Find space</a></li>
+                <li><a href="#">Share space</a></li>
+                <li><a href="#">Promote space</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
